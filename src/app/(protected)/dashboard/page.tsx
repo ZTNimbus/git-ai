d66c503cc6be1e856a42e895df00b0a66d7da5e2
nimbus <zerotonimbus@gmail.com>
2025-05-21 17:03:05 +0300

    build dashboard skeleton

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,39 @@
 "use client";
 
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import useProject from "~/hooks/use-project";
 
 function Dashboard() {
   const { project } = useProject();
   return (
     <div>
-      <h1>{project?.name}</h1>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="bg-primary w-fit rounded-md px-4 py-3">
+          <div className="flex items-center">
+            {" "}
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-4"></div>
+
+        <div className="flex items-center gap-4">
+          Team Members Invite Button Archive Button
+        </div>
+      </div>
     </div>
   );
 }
